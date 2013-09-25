4ac695e437c99bb050510acef85bfa956af3c9bc
Jared De Blander <jared@deblander.org>
2013-09-25 00:23:53 -0400

    Trying distribute instead of setup tools

--- spin.sh
+++ spin.sh
@@ -23,8 +23,8 @@ ufw allow 80
 ufw enable
 
 # Get ez_isntall and pip running
-wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
-wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-
-python ez_setup.py
-python get-pip.py
+curl -O http://python-distribute.org/distribute_setup.py
+python distribute_setup.py
+easy_install pip
+pip install virtualenv
+pip install virtualenvwrapper
