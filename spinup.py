88f7d172d9ec131bc691bc48c9ec9c3d1a3e0955
Jared De Blander <jared@deblander.org>
2013-09-25 00:31:57 -0400

    Removed python version

--- spinup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import libspin
-
-libspin.apt_upgrade()
-libspin.install_package('htop')
-libspin.install_package('iftop')
-libspin.install_package('nginx')
-libspin.install_package('ufw')
