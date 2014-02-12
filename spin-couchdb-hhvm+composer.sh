c509636822ae272c41fd7b61b0d1bd9bea8f6018
Jared De Blander <jared@deblander.org>
2014-02-12 00:32:45 -0500

    Updated CHC stack

--- spin-couchdb-hhvm+composer.sh
+++ spin-couchdb-hhvm+composer.sh
@@ -6,7 +6,7 @@ wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
 echo deb http://dl.hhvm.com/ubuntu precise main | sudo tee /etc/apt/sources.list.d/hhvm.list
 
 apt-get update -y && apt-get upgrade -y
-apt-get install couchdb php5 hhvm -y
+apt-get install couchdb php5-cli hhvm -y
 
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
