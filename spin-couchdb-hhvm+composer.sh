706b359d07910788fee93c0abf5bd2385f3dde1a
Jared De Blander <jared@deblander.org>
2014-02-11 23:00:19 -0500

    Updated couched installer

--- spin-couchdb-hhvm+composer.sh
+++ spin-couchdb-hhvm+composer.sh
@@ -3,7 +3,7 @@
 # CouchDB + HHVM for Ubuntu 12.04 LTS
 
 apt-get update -y && apt-get upgrade -y
-apt-get install couchdb
+apt-get install couchdb -y
 wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
 echo deb http://dl.hhvm.com/ubuntu precise main | sudo tee /etc/apt/sources.list.d/hhvm.list
 apt-get update -y && apt-get install hhvm -y
