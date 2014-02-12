27c3494730b438345aa1e22b044abd9361fa8af9
Jared De Blander <jared@deblander.org>
2014-02-11 23:12:54 -0500

    Further CHC updates

--- spin-couchdb-hhvm+composer.sh
+++ spin-couchdb-hhvm+composer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# CouchDB + HHVM for Ubuntu 12.04 LTS
+# CouchDB + HHVM + Composer for Ubuntu 12.04 LTS
 
-apt-get update -y && apt-get upgrade -y
-apt-get install couchdb -y
 wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
 echo deb http://dl.hhvm.com/ubuntu precise main | sudo tee /etc/apt/sources.list.d/hhvm.list
-apt-get update -y && apt-get install hhvm -y
+
+apt-get update -y && apt-get upgrade -y
+apt-get install couchdb php5 hhvm -y
+
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
