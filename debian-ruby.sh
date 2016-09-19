42d842961b488b8d0b9011ac11a1e0b0475c2cec
Jared <jared@quantumcodeworks.com>
2016-09-18 23:24:09 -0400

    Create debian-ruby.sh

---
+++ debian-ruby.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Do basic debian setup
+source <(curl -s https://raw.githubusercontent.com/jared0x90/dotfiles/master/setup-debian.sh)
+
+# Install rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+
+source /etc/profile.d/rvm.sh
